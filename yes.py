fd4096144f00a786c0ccddcf4faa9e1fe68267d6
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-12-07 11:57:18 +0000

    джячсдмдьч

--- yes.py
+++ yes.py
@@ -5,16 +5,17 @@ from matplotlib.animation import FuncAnimation
 
 
 G = 6.67 * 10 ** (-11)
-M = 1.98 * 10 ** (30)
+M = 900 * 1.98 * 10 ** (30)
 c = 3e8
 R = 149 * 10**9
 Rs = 696340000
+eccentricity = 0.8
 
 # некая частица 1
 x0e = 0.37499 * R
 vx0e = 0
 y0e = 0
-vy0e = 58980
+vy0e =  np.sqrt(G * M / x0e * (1 + eccentricity))
 
 # некая частица 2
 x0m = 0
@@ -23,14 +24,12 @@ y0m = 2 * 149 * 10 ** 9
 vy0m = 0
 
 aph = 0.466697 * R
-Va = 38860
-l = x0e * vy0e - y0e * vy0e
-# l = (x0e * vy0e + aph * Va) / 2
-p = (x0e**2 + y0e**2)**0.5 / Rs
+Va = np.sqrt(G * M / x0e * (1 - eccentricity))
+l = (x0e * vy0e + aph * Va) / 2
 plt.style.use('dark_background')
 frames = 500
 T = 365 * 24 * 60 * 60  # seconds_in_year
-years = 8
+years = 0.08
 t = np.linspace(0, years * T, frames)
 
 
@@ -38,7 +37,7 @@ def move_func(s, t):
     (x1, vx1, y1, vy1,
      xm, vxm, ym, vym) = s
 
-    # p = (x1**2 + y1**2)**0.5 / Rs
+    p = (x1**2 + y1**2)**0.5 / Rs
     # l = (x1 * vy1 + y1 * vx1) / 2
 
     dxdt1 = vx1
