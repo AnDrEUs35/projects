db47ad53e6c8aa0cdadd6f333038b8b970e06258
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2025-01-04 15:54:43 +0000

    asdadasdsadasdasd

--- project_BH.py
+++ project_BH.py
@@ -3,44 +3,45 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# Задаём
-x0 = 46001009
-y0 = 0
-try:
-    if x0 == 0:
-        raise ZeroDivisionError
-    else:
-        phi = np.arctan(y0 / x0)
-except ZeroDivisionError:
-    phi = 0
-
-v_merc = 47.36
-ecc = 0.205
-
-
-def f(r, t, params):
-    r1, r2, r3 = r
-    l = params
+def f(r, t, L1):
+    r1, r2, r3,= r
+    l = L1
     return [r2, -1/(2*r1**2)+l**2/r1**3-1*1.5*l**2/r1**4, l/r1**2]
 
-# МОЖНО ИССЛЕДОВАТЬ параметры: r0, params
+# Задаём
+x0 = 46001009 # km
+y0 = 0
+Vx = 0
+Vy = 47.36 # km / s
+ecc = 0.205
+
+phi = np.arctan2(y0, x0)
+alpha = np.arctan2(Vy, Vx)
+
 # r_g = 2 * G * M / c**2
 r_g_sun = 2.95 # km
 c = 2.99e5
 r_merc = np.sqrt(x0**2 + y0**2) / r_g_sun
-r0 =[r_merc, 0, phi] # В единицах гравитационного радиуса
 N = 300
 
-v_per = v_merc * np.sqrt((1 + ecc) / (1 - ecc)) / c
+v_merc = np.sqrt(Vx**2 + Vy**2) / c
+v_thau = v_merc * np.sin(phi - alpha) # тангенциальная скорость
+v_r = v_merc * np.cos(phi - alpha) # радиальная скорость
 
-L1 = v_per * r_merc # момент импульса для круговой орбиты
-params = 1.0 * L1 # если взять больше, то орбита еще вытянется
+
+# v_per = v_merc * np.sqrt((1 + ecc) / (1 - ecc)) / c # тоже тангенциальная скорость
+
+L1 = v_thau * r_merc # момент импульса для круговой орбиты
+
+r0 =[r_merc, v_r, phi] # Расстояние, радиальная компонента скорость, угол
 time = 2 * np.pi * r0[0]**2 / L1 # период обращения по круговой орбите
 
 t = np.linspace(0, 15*time, N)
 
-sol = odeint(f, r0, t, args=(params,))
+sol = odeint(f, r0, t, args=(L1,))
 
+
+#--------------------------------------------------------------------------------------
 radius = sol[:,0]
 angle = sol[:,2]
 
